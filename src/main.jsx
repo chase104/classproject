5a725a27f8e9d1c7db59798675ae20360144e97a
Parul Jain <pjain@LABA.PSWAS.LOCAL>
2023-06-26 14:11:15 -0700

    Changed context file extension from js to jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import PrimaryContextProvider from "./contexts/PrimaryContext.js";
+import PrimaryContextProvider from "./contexts/PrimaryContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
