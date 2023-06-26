7b59eed9c7f6b889e9afdec9b525341ee80705d8
Vitalii <metalyx3@gmail.com>
2023-06-26 13:36:40 -0700

    Fetching people from SW API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  // You can use this state variable to manage network state (for showing loaders / errors etc.)
+  const [fetchingPeople, setFetchingPeople] = useState({
+    isLoading: false,
+    isSuccess: false,
+    error: "",
+  });
 
+  // Once App component is mounted - this useEffect will be executed
+  useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        setFetchingPeople({
+          isLoading: true,
+          isSuccess: false,
+          error: "",
+        });
+
+        const response = await axios.get("https://swapi.dev/api/people");
+
+        setFetchingPeople({
+          isLoading: false,
+          isSuccess: true,
+          error: "",
+        });
+
+        // People here - is the array of people from API
+        const people = response.data.results;
+
+        // You can put it to context here...
+      } catch (e) {
+        setFetchingPeople({
+          isLoading: false,
+          isSuccess: false,
+          error: "An error occured while trying to fetch people...",
+        });
+      }
+    };
+
+    fetchPeople();
+  }, []);
 
   return (
     <>
       <h1>Our Class Project</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
