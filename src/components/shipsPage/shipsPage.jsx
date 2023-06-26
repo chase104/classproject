67d4ae4d17ae2dce385b06e4dfb9c581a0ef89ef
Bader Bricha <bbricha@LABA.PSWAS.LOCAL>
2023-06-26 15:21:40 -0700

    passing ships.name as param

--- src/components/shipsPage/shipsPage.jsx
+++ src/components/shipsPage/shipsPage.jsx
@@ -11,7 +11,7 @@ const shipsPage = () => {
 
     let shipsJsxArray = ships.map((object) => {
             
-            return <Link to={`/singleShip/${object.id}`}><h4 key={object.id}>{object.name}</h4></Link> 
+            return <Link to={`/singleShip/${object.name}`}><h4 key={object.id}>{object.name}</h4></Link> 
         }
     )
 
