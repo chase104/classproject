bce738baa1df8bc1f68b8fdea6f77269b3d689a9
Parul Jain <pjain@LABA.PSWAS.LOCAL>
2023-06-26 13:36:46 -0700

    Added Context file

---
+++ src/contexts/PrimaryContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+export const primaryContext = createContext();
+
+const PrimaryContextProvider = ({ children }) => {
+  // put state
+  const [people, setPeople] = useState([]);
+  const [ships, setShip] = useState([]);
+
+  let stateObject = {
+    people,
+    setPeople,
+    ships,
+    setShip,
+  };
+
+  return (
+    // put state into provider
+    <primaryContext.Provider value={stateObject}>
+      {children}
+    </primaryContext.Provider>
+  );
+};
+
+export default PrimaryContextProvider;
