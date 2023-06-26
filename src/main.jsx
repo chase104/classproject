5686949f78161b88212bf26b106e15e0f09f4c9b
Sataporn Worasilpchai <sworasilpchai@LABA.PSWAS.LOCAL>
2023-06-26 13:46:50 -0700

    try again

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <Router/>
       <App />
     <Router/>
