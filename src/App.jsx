0bea8047ac13a831af6bb521013ca25485b172c0
Sataporn Worasilpchai <sworasilpchai@LABA.PSWAS.LOCAL>
2023-06-26 14:08:50 -0700

    routes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
 
   return (
     <>
       <h1>Our Class Project</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/ships" element={<Ships/>} />
+          <Route path="/singleships" element={<Singleships/>} />
+          <Route path="/people" element={<People/>} />
+          <Route path="/singlePeople" element={<SinglePeople/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
