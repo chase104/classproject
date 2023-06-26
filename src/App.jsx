95e22ac15bba8283c3dbf0e28cc303c6ce7d1b67
samtessema9 <99148321+samtessema9@users.noreply.github.com>
2023-06-26 13:46:15 -0700

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-
 import './App.css'
+import {useEffect, useContext} from 'react';
+import primaryContext from './contexts/PrimaryContext.js';
+import axios from 'axios';
 
 function App() {
 
+  const {setShips} = useContext(primaryContext)
+
+  useEffect(() => {
+    try {
+      const getShips = async () => {
+        const response = await axios.get("https://swapi.dev/api/starships")
+        const ships = response.data.results
+        setShips(ships)
+      }
+  
+      getShips()
+    }
+    catch (error) {
+      console.error(error)
+    }    
+
+  }, [])
 
   return (
     <>
