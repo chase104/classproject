4126856de7231b1ba53c8975f308a7e8fc6d3b62
Tri Tran <ttran@LABA.PSWAS.LOCAL>
2023-06-26 14:05:31 -0700

    updated CSS

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.navbar}>
       <img
+        className={styles.logo}
         src="https://seeklogo.com/images/S/star-wars-logo-80607D2AC5-seeklogo.com.png"
         alt="starwars logo"
       ></img>
 
-      <Link to="/">Home</Link>
-      <Link to="/ships">Ships</Link>
-      <Link to="/singleShips">Single Ships</Link>
-      <Link to="/people">People</Link>
-      <Link to="/singlePeople">Single People</Link>
+      <Link className={styles.link} to="/">Home</Link>
+      <Link className={styles.link} to="/ships">Ships</Link>
+      <Link className={styles.link} to="/singleShips">Single Ships</Link>
+      <Link className={styles.link} to="/people">Single People</Link>
+      <Link className={styles.link} to="/singleeople">Single People</Link>
     </div>
   );
 };
