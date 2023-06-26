03eaa0c9d5066085b1203fdbf9da9d2177861188
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-06-26 14:06:42 -0700

    deleted singleShip branch and made 9-Single Ship Page branch

--- src/sShip/index.jsx
+++ src/sShip/index.jsx
@@ -3,14 +3,14 @@ import { PrimaryContext } from './PrimaryContext'
 
 
 const singleShip = () => {
-    const { singleShip } = useContext(PrimaryContext);
+    const { ships } = useContext(PrimaryContext);
 
     return(
         <div>
             <h1>Single Ship Details</h1>
             <p>Ship Name: {singleShip.name}</p>
-            <p>Model {singleShip.model}</p>
-            <p>Class {singleShip.starship_class}</p>
+            <p>Model: {singleShip.model}</p>
+            <p>Class: {singleShip.starship_class}</p>
 
         </div>
     )
